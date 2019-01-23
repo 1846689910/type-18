cd6b6091b03f24e2a5c5c66e97658e75a635c82f
SyracuseWiesler <gslzwiesler@gmail.com>
2019-01-22 23:44:11 -0800

    router config

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -3,7 +3,7 @@
  */
 import React from "react";
 import ReactDOM from "react-dom";
-import {BrowserRouter as Router, Route, Switch, Link, Redirect} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import "../images/favicon.ico";
 import "../../node_modules/leaflet/dist/leaflet.css";
 import "../../node_modules/leaflet/dist/images/marker-icon.png";
@@ -23,28 +23,33 @@ import "../../node_modules/jquery-ui-dist/jquery-ui.structure.min.css";
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../../node_modules/bootstrap/dist/js/bootstrap.min";
 import store from "./settings/store";
-import ReduxConcise from "./redux/ReduxConcise";
-import ReactReduxConcise from "./react-redux/ReactReduxConcise";
-import ReselectConcise from "./reselect/ReselectConcise";
-import MobXConcise from "./mobx/MobXConcise";
 import {Provider} from "react-redux";
-import Main, {StatelessComp} from "./Main";
+import {routes} from "./settings/routes";
+import {renderRoutes} from "react-router-config";
 
 // ReactDOM.render(<Main />, document.querySelector("#root"));
+// const render = () => ReactDOM.render(
+//     <Router>
+//         <Switch>
+//             <Route path="/redux"><ReduxConcise store={store}/></Route>
+//             <Route path="/react-redux">
+//                 <Provider store={store}><ReactReduxConcise/></Provider>
+//             </Route>
+//             <Route path="/reselect">
+//                 <Provider store={store}><ReselectConcise/></Provider>
+//             </Route>
+//             <Route path="/mobx" component={MobXConcise}/>
+//             <Route path="/" component={Main}/>
+//         </Switch>
+//     </Router>,document.querySelector("#root")
+// );
+const App = () => renderRoutes(routes);
 const render = () => ReactDOM.render(
-    <Router>
-        <Switch>
-            <Route path="/redux"><ReduxConcise store={store}/></Route>
-            <Route path="/react-redux">
-                <Provider store={store}><ReactReduxConcise/></Provider>
-            </Route>
-            <Route path="/reselect">
-                <Provider store={store}><ReselectConcise/></Provider>
-            </Route>
-            <Route path="/mobx" component={MobXConcise}/>
-            <Route path="/" component={Main}/>
-        </Switch>
-    </Router>,document.querySelector("#root")
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>,document.querySelector("#root")
 );
 render();
 store.subscribe(render);
