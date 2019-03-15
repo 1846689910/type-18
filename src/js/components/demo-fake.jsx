9fdcef7495d090e35d3aada55daa3975b8177062
1846689910 <38680030+1846689910@users.noreply.github.com>
2019-03-15 10:21:19 -0700

    Demo Dynamic Import (#19)

---
+++ src/js/components/demo-fake.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+export const Fake = () => (
+  <div>
+    <strong>Fake Comp Here</strong>
+  </div>
+);
+export default () => (
+  <div>
+    <strong>Fake Comp Here</strong>
+  </div>
+);
