b2b62baf0de93418c5845647231e268debdfd0f8
1846689910 <eric14706@gmail.com>
2019-08-11 16:02:02 -0700

    mobx decorator error

--- src/client/.babelrc.js
+++ src/client/.babelrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   extends: "funny-react-app-archetype/config/babel/babelrc-client.js",
-  presets: ["babel-preset-mobx"]
+  // presets: ["babel-preset-mobx"],
+  plugins: [
+    ["@babel/plugin-proposal-decorators", { legacy: true }],
+    ["@babel/plugin-proposal-class-properties", { loose: true }]
+  ]
 };
