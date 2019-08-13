67d1ce296766f395b4da6928aa3b2f4c6c81d96d
1846689910 <eric14706@gmail.com>
2019-08-12 22:53:14 -0700

    rm unnecessary files

--- temp/script.js
+++
@@ -1,9 +0,0 @@
-class People {
-    constructor() {
-        this.walk = this.walk.bind(this);
-    }
-    run = () => console.log("running");
-    walk() {
-        console.log("walking");
-    }
-}
