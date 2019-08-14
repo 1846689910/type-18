99a417c87922a27a7a74a03882af28d7e1d5ea4b
1846689910 <eric14706@gmail.com>
2019-08-13 22:07:52 -0700

    fix

--- clis/cli.js
+++ clis/cli.js
@@ -3,18 +3,22 @@ const logger = require("funny-react-app-archetype/config/utils/color-logger");
 const method = process.argv[2];
 
 const baseCommands = {
-  runDevServer: "webpack-dev-server --config webpack.config.dev.js --progress --color"
+  runDevServer: `$\"webpack-dev-server\" --config webpack.config.dev.js --progress --color` // eslint-disable-line
 };
 
 const methods = {
   runDev() {
     shell.exec(baseCommands.runDevServer, {
-      env: { NODE_ENV: "development" }
+      env: { NODE_ENV: "development", PATH: process.env.PATH + ":/usr/local/bin" }
     });
   },
   runProd() {
     shell.exec(baseCommands.runDevServer, {
-      env: { NODE_ENV: "production", MOCK_PROD_IN_DEV: "true" }
+      env: {
+        NODE_ENV: "production",
+        MOCK_PROD_IN_DEV: "true",
+        PATH: process.env.PATH + ":/usr/local/bin"
+      }
     });
   }
 };
