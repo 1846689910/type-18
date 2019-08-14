d2f4f3c1089410f7187cd635dbb08fbb9c43eb45
1846689910 <eric14706@gmail.com>
2019-08-14 16:01:14 -0700

    mod template

--- archetype/config/index.js
+++ archetype/config/index.js
@@ -1,6 +1,16 @@
 module.exports = {
   webpack: {
-    enableShortenCSSNames: true
+    enableShortenCSSNames: true,
+    htmlWebpackPluginOptions: {
+      unbundled: {
+        pre: {
+          js: ["<script type='text/javascript'>console.log('pre-js')</script>"]
+        },
+        post: {
+          js: ["<script type='text/javascript'>console.log('post-js')</script>"]
+        }
+      }
+    }
   },
   babel: {
     enableTypeScript: true
