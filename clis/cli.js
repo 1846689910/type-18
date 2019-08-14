574d3208f9c5e8bcc28fd438a719f09c94dafc9a
1846689910 <eric14706@gmail.com>
2019-08-13 22:20:26 -0700

    mod

--- clis/cli.js
+++ clis/cli.js
@@ -2,23 +2,27 @@ const shell = require("shelljs");
 const logger = require("funny-react-app-archetype/config/utils/color-logger");
 const method = process.argv[2];
 
+const isWin32 = process.platform === "win32";
+const setEnv = env => ({ ...env, ...(!isWin32 && { PATH: process.env.PATH + ":/usr/local/bin" }) });
+
+const webpackDevServer = isWin32 ? "webpack-dev-server" : '$"webpack-dev-server"'; // eslint-disable-line
+
 const baseCommands = {
-  runDevServer: `$\"webpack-dev-server\" --config webpack.config.dev.js --progress --color` // eslint-disable-line
+  runDevServer: `${webpackDevServer} --config webpack.config.dev.js --progress --color`
 };
 
 const methods = {
   runDev() {
     shell.exec(baseCommands.runDevServer, {
-      env: { NODE_ENV: "development", PATH: process.env.PATH + ":/usr/local/bin" }
+      env: setEnv({ NODE_ENV: "development" })
     });
   },
   runProd() {
     shell.exec(baseCommands.runDevServer, {
-      env: {
+      env: setEnv({
         NODE_ENV: "production",
-        MOCK_PROD_IN_DEV: "true",
-        PATH: process.env.PATH + ":/usr/local/bin"
-      }
+        MOCK_PROD_IN_DEV: "true"
+      })
     });
   }
 };
