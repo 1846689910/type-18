afe56f1a0387a1b954f989c4b4d0e54e0758cb04
1846689910 <38680030+1846689910@users.noreply.github.com>
2019-08-13 22:27:11 -0700

    Fix command line with win32 & darwin (#28)

--- clis/cli.js
+++ clis/cli.js
@@ -2,19 +2,28 @@ const shell = require("shelljs");
 const logger = require("funny-react-app-archetype/config/utils/color-logger");
 const method = process.argv[2];
 
+const isWin32 = process.platform === "win32";
+const setEnv = env => ({ ...env, ...(!isWin32 && { PATH: process.env.PATH + ":/usr/local/bin" }) });
+const baseCommand = cmd => (isWin32 ? cmd : `$"${cmd}"`);
+
 const baseCommands = {
-  runDevServer: "webpack-dev-server --config webpack.config.dev.js --progress --color"
+  runDevServer: `${baseCommand(
+    "webpack-dev-server"
+  )} --config webpack.config.dev.js --progress --color`
 };
 
 const methods = {
   runDev() {
     shell.exec(baseCommands.runDevServer, {
-      env: { NODE_ENV: "development" }
+      env: setEnv({ NODE_ENV: "development" })
     });
   },
   runProd() {
     shell.exec(baseCommands.runDevServer, {
-      env: { NODE_ENV: "production", MOCK_PROD_IN_DEV: "true" }
+      env: setEnv({
+        NODE_ENV: "production",
+        MOCK_PROD_IN_DEV: "true"
+      })
     });
   }
 };
