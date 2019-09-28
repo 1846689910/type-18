d2a41107ea2c1ea27f1c4d1eb1862b1bd1238ee5
1846689910 <eric14706@gmail.com>
2019-09-27 22:56:35 -0700

    mod

--- src/client/js/components/demo-home.jsx
+++ src/client/js/components/demo-home.jsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import Nav from "./Nav";
+import Carousel from "./Carousel";
 import bs from "bootstrap/dist/css/bootstrap.min.css";
 
 class DemoHome extends React.Component {
@@ -13,7 +14,9 @@ class DemoHome extends React.Component {
     const { dispatch, route } = this.props;
     return <div style={{display: "flex", flexFlow: "column wrap", justifyContent: "center"}}>
       <Nav route={route}/>
-      <div style={{alignSelf: "center", width: "80%", textAlign: "center", height: "200px", margin: "10px 0", background: "gray"}}>Carousel</div>
+      <div style={{ alignSelf: "center", width: "80%", textAlign: "center" }}>
+        <Carousel />
+      </div>
       <div style={{alignSelf: "center", width: "80%", textAlign: "center", height: "200px", margin: "10px 0", background: "gray"}}>World</div>
       <div style={{alignSelf: "center", width: "80%", textAlign: "center", height: "200px", margin: "10px 0", background: "gray"}}>Row</div>
       <div style={{alignSelf: "center", width: "80%", textAlign: "center", height: "200px", margin: "10px 0", background: "gray"}}>Row</div>
