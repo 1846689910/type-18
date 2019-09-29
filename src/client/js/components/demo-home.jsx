be81f4f86fe618af81282d2215d6b6a10232bd10
1846689910 <eric14706@gmail.com>
2019-09-29 00:11:04 -0700

    add demo-dynamic-import

--- src/client/js/components/demo-home.jsx
+++ src/client/js/components/demo-home.jsx
@@ -6,6 +6,7 @@ import Nav from "./Nav";
 import DemoForm from "./demo-form";
 import Carousel from "./Carousel";
 import bs from "bootstrap/dist/css/bootstrap.min.css";
+import DemoDynamicImport from "./demo-dynamic-import";
 
 class DemoHome extends React.Component {
   constructor(props) {
@@ -21,8 +22,10 @@ class DemoHome extends React.Component {
       <div style={{alignSelf: "center", width: "80%", textAlign: "center", height: "200px", margin: "10px 0", display: "flex", flexFlow: "row wrap", justifyContent: "center"}}>
         <DemoForm/>
       </div>
-      <div style={{alignSelf: "center", width: "80%", textAlign: "center", height: "200px", margin: "10px 0", background: "gray"}}>Row</div>
-      <div style={{alignSelf: "center", width: "80%", textAlign: "center", height: "200px", margin: "10px 0", background: "gray"}}>Row</div>
+      <div style={{alignSelf: "center", width: "80%", textAlign: "center", height: "200px", margin: "10px 0", padding: "40px 0"}}>
+        <h4>Dynamic Import Display</h4>
+        <DemoDynamicImport/>
+      </div>
     </div>;
   }
 }
